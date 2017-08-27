d98e649522f9606e03315e8ce8ca047140f43203
kaktus83 <adamowicz.anna@gmail.com>
2017-08-28 00:15:58 +0200

    first commit

---
+++ App.js
@@ -0,0 +1,34 @@
+var baseUrl = 'https://kodilla.com/pl/bootcamp-api';
+var myHeaders = {
+  'X-Client-Id': '2240',
+  'X-Auth-Token': '1c31a0930606b6fb95ce5e736172fb75'
+};
+
+$.ajaxSetup({
+	headers: myHeaders
+});
+
+$.ajax({
+    url: baseUrl + '/board',
+    method: 'GET',
+    success: function(response) {
+      setupColumns(response.columns);
+    }
+});
+
+function setupColumns(columns) {
+    columns.forEach(function (column) {
+  		var col = new Column(column.id, column.name);
+        board.createColumn(col);
+        setupCards(col, column.cards);
+    });
+}
+
+function setupCards(col, cards) {
+	cards.forEach(function (card) {
+        var card = new Card(card.id, card.name, card.bootcamp_kanban_column_id);
+    	col.createCard(card);
+  	})
+}
+
+createCard();
